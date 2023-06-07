a42a67c226a5daa469974f72fabbd7e3e67a6716
apreziosi1 <a.preziosi@outlook.it>
2023-06-07 20:37:40 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,61 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <main>
+    <header>
+      <img src="./assets/logo.png" alt="Vue-logo">
+      <h1>Vue + Pinia</h1>
+    </header>
+
+    <div class="new-task-form">
+      <TaskForm />
+    </div>
+
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+      <button @click="taskStore.$reset">Reset tasks</button>
+    </nav>
+
+    <div class="loading" v-if="loading">
+      Loading...
+    </div>
+
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ totalCount }} tasks left to do</p>
+      <div v-for="task in tasks" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ favCount }} fav tasks left to do</p>
+      <div v-for="task in favs" :key="task.id">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+  </main>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { ref } from 'vue'
+import './assets/main.css'
+import TaskDetails from './components/TaskDetails.vue'
+import { useTaskStore } from './stores/TaskStore'
+import TaskForm from './components/TaskForm.vue'
+import { storeToRefs } from 'pinia'
 
-nav {
-  padding: 30px;
-}
+export default {
+  components: { TaskDetails, TaskForm },
+  setup() {
+    const taskStore = useTaskStore()
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+    const { tasks, loading, totalCount, favCount } = storeToRefs(taskStore)
 
-nav a.router-link-exact-active {
-  color: #42b983;
+    taskStore.getTasks()
+
+    const filter = ref('all')
+
+    return { taskStore, filter }
+  }
 }
-</style>
+</script>
